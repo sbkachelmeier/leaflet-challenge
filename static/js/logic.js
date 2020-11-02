0a6ef4f63fb38debbbfd2f705c115c1271454c3d
Sarah Kachelmeier <sbkachelmeier@gmail.com>
2020-11-01 20:13:17 -0600

    Completed assignment

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,7 +6,7 @@ var myMap = L.map("myMap", {
     zoom: 5
   });
 
-// // Add tile layer?
+// // Add tile layer
 
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
